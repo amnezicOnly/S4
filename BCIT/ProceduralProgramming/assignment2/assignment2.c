f214084e2027f8a5125976dc859b85a542008fea
Antoine Lévêque <antoine.leveque@epita.fr>
2024-02-23 10:54:54 -0800

    fin dm2 C + devoir Linux sur Vim

--- BCIT/ProceduralProgramming/assignment2/assignment2.c
+++ BCIT/ProceduralProgramming/assignment2/assignment2.c
@@ -8,24 +8,23 @@
 #include<err.h>
 
 struct Particule{
-	short X;	// position en X
-	short Y;	// position en Y
-	short velX;	// à chaque période : X+=velX
-	short velY;	// à chaque période : Y+=velY
+	short X;	// X position
+	short Y;	// Y position
+	short velX;	// X direction
+	short velY;	// Y direction
 };
 
-short getValue(FILE* input){	// renvoie la première valeur en int depuis le début du fichier à partir du pointeur
+short getValue(FILE* input){
 	int res = 0;
-    	int negative = 1;   // si le nombre est négatif
-    	int state = 1;  // indique si on a déjà lu un nombre
-	int state2 = 1;
+   	int negative = 1;
+    	int state = 1;
 	char temp;
 	while((temp=fgetc(input))!=EOF && temp!=',' && temp!='\n' && temp!='.'){
         	if(temp=='-' && negative==1){
-            		negative=0;    // indique que le résultat retourné sera négatif
+            		negative=0;
 		}
         	else if(temp=='-' && negative==0)
-            		return -500;    // si on déjà lu le signe -
+            		return -500;
 
         	else if(temp>='0' && temp<='9'){
         		state = 0;
@@ -34,19 +33,12 @@ short getValue(FILE* input){	// renvoie la première valeur en int depuis le dé
 	}
 
 	if(temp==EOF || (temp==',' && state==1) || (temp=='\n' && state==1) || temp=='.')
-        // si le fichier est vide ou que les coordonnées ne sont pas mises coorectement
 		return -500;
 	if(negative==0)
         	return (short)((-1)*res);
     	return (short)res;
 }
 
-void printListe(struct Particule* liste, size_t size){
-	printf("Liste : ");
-	for(size_t i=0; i<size; i++)
-		printf("(X: %hd, Y: %hd, velX: %hd, velY: %hd)-->",liste[i].X,liste[i].Y,liste[i].velX,liste[i].velY); 
-	printf("NULL (%zu)\n",size);
-}
 
 short searchParticule(struct Particule* liste, size_t size, short x, short y){
 	for(size_t i=0; i<size; i++){	// si la particule existe déjà, on ne l'ajoute pas
@@ -57,24 +49,18 @@ short searchParticule(struct Particule* liste, size_t size, short x, short y){
 }
 
 void addParticule(struct Particule* liste, size_t* size, short x, short y, short velx, short vely){
-	// la liste n'est pas vide
-	if(searchParticule(liste,*size,x,y)==-1){
-		// on ne rajoute une particule que si elle n'est pas présente dans la liste
-		(*size)++;
-		liste = realloc(liste,(*size)*sizeof(struct Particule));
-		liste[(*size)-1].X=x;
-		liste[(*size)-1].Y=y;
-		liste[(*size)-1].velX=velx;
-		liste[(*size)-1].velY=vely;
-		
-	}
+	(*size)++;
+	liste = realloc(liste,(*size)*sizeof(struct Particule));
+	liste[(*size)-1].X=x;
+	liste[(*size)-1].Y=y;
+	liste[(*size)-1].velX=velx;
+	liste[(*size)-1].velY=vely;
 }
 
 void play(struct Particule* liste, size_t size, short width, short height){
-	// va déplacer chaque particule de velX et velY
     	for(size_t i = 0; i<size; i++){
     		int state = 0;
-        	if(liste[i].X+liste[i].velX<=0 || liste[i].X+liste[i].velX>=width-1){   // cas où une colonne est touchée
+        	if(liste[i].X+liste[i].velX<=0 || liste[i].X+liste[i].velX>=width-1){
             		if(liste[i].X+liste[i].velX==0 || liste[i].X+liste[i].velX==width-1){
                 		liste[i].X += liste[i].velX;
             		}
@@ -90,7 +76,7 @@ void play(struct Particule* liste, size_t size, short width, short height){
         
         	}
         
-        	if(liste[i].Y+liste[i].velY<=0 || liste[i].Y+liste[i].velY>=height-1){   // cas où une ligne est touchée
+        	if(liste[i].Y+liste[i].velY<=0 || liste[i].Y+liste[i].velY>=height-1){
             		if(liste[i].Y+liste[i].velY==0 || liste[i].Y+liste[i].velY==height-1){
                 		liste[i].Y += liste[i].velY;
             		}
@@ -119,11 +105,8 @@ void play(struct Particule* liste, size_t size, short width, short height){
 }
 
 void checkCollision(struct Particule* liste,size_t size){
-	// on vérifie que plusieurs particules ne sont pas sur la même coordonnée
-	// si oui, on modifie les coordonnées de toutes ces occurences à 25 et -2
 	size_t i = 0;
     	while(i<size){
-		// si on croise deux particules qui ont les mêmes coordonnées
 		int state = 1;
 		if(liste[i].X!=25 && liste[i].Y!=-2){
 			for(int j=i+1; j<size; j++){
@@ -147,18 +130,15 @@ void writeOutput(FILE* output, struct Particule* liste, size_t size, short width
 		fputc('*',output);
 	fputc('\n',output);
 	for(short i = height-1; i>=0; i--){
-		// pour chaque ligne (de height-1 à 0)
-		fputc('*',output);	// colonne
+		fputc('*',output);
 		for(short j = 0; j<width; j++){
-			// pour chaque colonne (de 0 à width-1)
 			int temp = searchParticule(liste,size,j,i);
-			// Si (j,i) existe 
 			if(temp!=-1)
 				fputc('+',output);
 			else
 				fputc(' ',output);
 		}
-		fputc('*',output);	// colonne
+		fputc('*',output);
 		fputc('\n',output);
 	}
 	for(int i=-1; i<=width; i++)
@@ -183,9 +163,9 @@ int main(int argc, char** argv){
         	return 0;
     	}
     
-	short width = getValue(input);	// récupère la largeur de la fenêtre à partir de l'input
-	short height = getValue(input);	// récupère la hauteur de la fenêtre à partir de l'input
-	short lapNumber = getValue(input);	// récupère le nombre de tours
+	short width = getValue(input);
+	short height = getValue(input);
+	short lapNumber = getValue(input);
 
     if(width<1 || height<1 || width>20 || height>20){
         fputs("Error\n",output);
@@ -193,8 +173,7 @@ int main(int argc, char** argv){
         fclose(output);
         return 0;
     }
-
-    // Récupération des infos de la première particule    
+     
 	short x = getValue(input);
 	short y = getValue(input);
 	short velx = getValue(input);
@@ -207,7 +186,6 @@ int main(int argc, char** argv){
         return 0;
     }
 
-	// création de la liste et ajout de la première particule
 	struct Particule* liste = malloc(sizeof(struct Particule));
 	size_t* size = malloc(sizeof(size_t));
 	(*size) = 1;
@@ -217,17 +195,15 @@ int main(int argc, char** argv){
 	liste[0].velY=vely;
 	
 	short temp = getValue(input);
-    	// ajout successif des particules dans liste à partir de l'input
 	while(temp!=-500){
 		x = temp;
 		y = getValue(input);
 		velx = getValue(input);
 		vely = getValue(input);
-        	//printListe(liste,(*size));
-        	// s'il y a un problème dans les coordonnées
         	if(x<0 || x>=width || y<0 || y>=height || velx==-500 || vely==-500){
            		fputs("Error\n",output);
 			free(liste);
+			free(size);
             		fclose(input);
             		fclose(output);
             		return 0;
@@ -236,18 +212,16 @@ int main(int argc, char** argv){
 		temp = getValue(input);
 	}
 	fclose(input);
-
-	//printListe(liste,*size);
+	
 	for(short i = 0; i<lapNumber; i++){
 		play(liste,*size,width,height);
 		checkCollision(liste,*size);		
 	}
 
 
-	
-	// écrit la fenêtre et les particules dans le fichier output
 	writeOutput(output,liste,*size,width,height);	
    	free(liste);
+   	free(size);
 	fclose(output);
 		
 	return 0;
--- BCIT/ProceduralProgramming/assignment2/test.c
+++ BCIT/ProceduralProgramming/assignment2/test.c
@@ -10,21 +10,21 @@
 struct Particule{
 	short X;	// position en X
 	short Y;	// position en Y
-	short velX;	// a chaque periode : X+=velX
-	short velY;	// a chaque periode : Y+=velY
+	short velX;	// à chaque période : X+=velX
+	short velY;	// à chaque période : Y+=velY
 };
 
-short getValue(FILE* input){	// renvoie la premiere valeur en int depuis le debut du fichier a partir du pointeur
+short getValue(FILE* input){	// renvoie la première valeur en int depuis le début du fichier à partir du pointeur
 	int res = 0;
-    	int negative = 1;   // si le nombre est negatif
-    	int state = 1;  // indique si on a deja lu un nombre
+   	int negative = 1;   // si le nombre est négatif
+    int state = 1;  // indique si on a déjà lu un nombre
 	char temp;
 	while((temp=fgetc(input))!=EOF && temp!=',' && temp!='\n' && temp!='.'){
         	if(temp=='-' && negative==1){
-            		negative=0;    // indique que le resultat retourne sera negatif
+            		negative=0;    // indique que le résultat retourné sera négatif
 		}
         	else if(temp=='-' && negative==0)
-            		return -500;    // si on deja lu le signe -
+            		return -500;    // si on déjà lu le signe -
 
         	else if(temp>='0' && temp<='9'){
         		state = 0;
@@ -33,7 +33,7 @@ short getValue(FILE* input){	// renvoie la premiere valeur en int depuis le debu
 	}
 
 	if(temp==EOF || (temp==',' && state==1) || (temp=='\n' && state==1) || temp=='.')
-        // si le fichier est vide ou que les coordonnees ne sont pas mises coorectement
+        // si le fichier est vide ou que les coordonnées ne sont pas mises coorectement
 		return -500;
 	if(negative==0)
         	return (short)((-1)*res);
@@ -48,7 +48,7 @@ void printListe(struct Particule* liste, size_t size){
 }
 
 short searchParticule(struct Particule* liste, size_t size, short x, short y){
-	for(size_t i=0; i<size; i++){	// si la particule existe deja, on ne l'ajoute pas
+	for(size_t i=0; i<size; i++){	// si la particule existe déjà, on ne l'ajoute pas
 		if(liste[i].X==x && liste[i].Y==y)
 			return (short)i;
 	}
@@ -57,8 +57,8 @@ short searchParticule(struct Particule* liste, size_t size, short x, short y){
 
 void addParticule(struct Particule* liste, size_t* size, short x, short y, short velx, short vely){
 	// la liste n'est pas vide
-	if(searchParticule(liste,*size,x,y)==-1){
-		// on ne rajoute une particule que si elle n'est pas presente dans la liste
+	//if(searchParticule(liste,*size,x,y)==-1){
+		// on ne rajoute une particule que si elle n'est pas présente dans la liste
 		(*size)++;
 		liste = realloc(liste,(*size)*sizeof(struct Particule));
 		liste[(*size)-1].X=x;
@@ -66,14 +66,14 @@ void addParticule(struct Particule* liste, size_t* size, short x, short y, short
 		liste[(*size)-1].velX=velx;
 		liste[(*size)-1].velY=vely;
 		
-	}
+	//}
 }
 
 void play(struct Particule* liste, size_t size, short width, short height){
-	// va deplacer chaque particule de velX et velY
+	// va déplacer chaque particule de velX et velY
     	for(size_t i = 0; i<size; i++){
     		int state = 0;
-        	if(liste[i].X+liste[i].velX<=0 || liste[i].X+liste[i].velX>=width-1){   // cas où une colonne est touchee
+        	if(liste[i].X+liste[i].velX<=0 || liste[i].X+liste[i].velX>=width-1){   // cas où une colonne est touchée
             		if(liste[i].X+liste[i].velX==0 || liste[i].X+liste[i].velX==width-1){
                 		liste[i].X += liste[i].velX;
             		}
@@ -89,7 +89,7 @@ void play(struct Particule* liste, size_t size, short width, short height){
         
         	}
         
-        	if(liste[i].Y+liste[i].velY<=0 || liste[i].Y+liste[i].velY>=height-1){   // cas où une ligne est touchee
+        	if(liste[i].Y+liste[i].velY<=0 || liste[i].Y+liste[i].velY>=height-1){   // cas où une ligne est touchée
             		if(liste[i].Y+liste[i].velY==0 || liste[i].Y+liste[i].velY==height-1){
                 		liste[i].Y += liste[i].velY;
             		}
@@ -118,11 +118,11 @@ void play(struct Particule* liste, size_t size, short width, short height){
 }
 
 void checkCollision(struct Particule* liste,size_t size){
-	// on verifie que plusieurs particules ne sont pas sur la meme coordonnee
-	// si oui, on modifie les coordonnees de toutes ces occurences a 25 et -2
+	// on vérifie que plusieurs particules ne sont pas sur la même coordonnée
+	// si oui, on modifie les coordonnées de toutes ces occurences à 25 et -2
 	size_t i = 0;
     	while(i<size){
-		// si on croise deux particules qui ont les memes coordonnees
+		// si on croise deux particules qui ont les mêmes coordonnées
 		int state = 1;
 		if(liste[i].X!=25 && liste[i].Y!=-2){
 			for(int j=i+1; j<size; j++){
@@ -146,10 +146,10 @@ void writeOutput(FILE* output, struct Particule* liste, size_t size, short width
 		fputc('*',output);
 	fputc('\n',output);
 	for(short i = height-1; i>=0; i--){
-		// pour chaque ligne (de height-1 a 0)
+		// pour chaque ligne (de height-1 à 0)
 		fputc('*',output);	// colonne
 		for(short j = 0; j<width; j++){
-			// pour chaque colonne (de 0 a width-1)
+			// pour chaque colonne (de 0 à width-1)
 			int temp = searchParticule(liste,size,j,i);
 			// Si (j,i) existe 
 			if(temp!=-1)
@@ -182,9 +182,9 @@ int main(int argc, char** argv){
         	return 0;
     	}
     
-	short width = getValue(input);	// recupere la largeur de la fenetre a partir de l'input
-	short height = getValue(input);	// recupere la hauteur de la fenetre a partir de l'input
-	short lapNumber = getValue(input);	// recupere le nombre de tours
+	short width = getValue(input);	// récupère la largeur de la fenêtre à partir de l'input
+	short height = getValue(input);	// récupère la hauteur de la fenêtre à partir de l'input
+	short lapNumber = getValue(input);	// récupère le nombre de tours
 
     if(width<1 || height<1 || width>20 || height>20){
         fputs("Error\n",output);
@@ -193,7 +193,7 @@ int main(int argc, char** argv){
         return 0;
     }
 
-    // Recuperation des infos de la premiere particule    
+    // Récupération des infos de la première particule    
 	short x = getValue(input);
 	short y = getValue(input);
 	short velx = getValue(input);
@@ -206,7 +206,7 @@ int main(int argc, char** argv){
         return 0;
     }
 
-	// creation de la liste et ajout de la premiere particule
+	// création de la liste et ajout de la première particule
 	struct Particule* liste = malloc(sizeof(struct Particule));
 	size_t* size = malloc(sizeof(size_t));
 	(*size) = 1;
@@ -216,45 +216,36 @@ int main(int argc, char** argv){
 	liste[0].velY=vely;
 	
 	short temp = getValue(input);
-    	// ajout successif des particules dans liste a partir de l'input
-	while(temp!=(short)'E' && temp!=-500){
+    	// ajout successif des particules dans liste à partir de l'input
+	while(temp!=-500){
 		x = temp;
 		y = getValue(input);
 		velx = getValue(input);
 		vely = getValue(input);
         	//printListe(liste,(*size));
-        	// s'il y a un probleme dans les coordonnees
+        	// s'il y a un problème dans les coordonnées
         	if(x<0 || x>=width || y<0 || y>=height || velx==-500 || vely==-500){
            		fputs("Error\n",output);
-				free(liste);
-            	fclose(input);
-            	fclose(output);
-            	return 0;
-			}
-		printListe(liste,*size);
+			free(liste);
+            		fclose(input);
+            		fclose(output);
+            		return 0;
+        	}
 		addParticule(liste,size,x,y,velx,vely);
 		temp = getValue(input);
 	}
-	printListe(liste,*size);
-	if (temp == -500) {
-		fputs("Error\n",output);
-		free(liste);
-		fclose(input);
-		fclose(output);
-		return 0;
-	}
-
 	fclose(input);
 
 	//printListe(liste,*size);
 	for(short i = 0; i<lapNumber; i++){
 		play(liste,*size,width,height);
+        printListe(liste,*size);
 		checkCollision(liste,*size);		
 	}
 
 
 	
-	// ecrit la fenetre et les particules dans le fichier output
+	// écrit la fenêtre et les particules dans le fichier output
 	writeOutput(output,liste,*size,width,height);	
    	free(liste);
 	fclose(output);
