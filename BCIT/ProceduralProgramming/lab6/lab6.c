64c885f20c79ab8d65a0c84f0a88f2d0d052669b
Antoine Lévêque <antoine.leveque@epita.fr>
2024-03-07 17:09:19 -0800

    oui

--- BCIT/ProceduralProgramming/lab6/lab6.c
+++ BCIT/ProceduralProgramming/lab6/lab6.c
@@ -1,12 +1,18 @@
 // A01410991
-//#include <stdio.h>
-//#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 void writeOutputOne(FILE* output, int shift){
 	// écrit les valeurs du minimum et du maximum en fonction de shift dans le fichier output
-	// close input et output
-	
+	printf("Hello there\n");
+	int number = 0;
+	for(int i=0; i<shift-1; i++){
+		number<<=1;
+		number++;
+	}
+	fprintf(output,"min : -%d    %X\n",number,number);
+		
 }
 
 
@@ -15,7 +21,7 @@ int main(int argc, char **argv){
 	// argc : number of argument from the command line
 	// argv[0] : name of the program
 	// argv[>0] : user input
-	if(argc!=3){
+	if(argc!=3)
 		perror("Wrong argument count, usage : ./a.out input.txt output.txt\n");
 	
 	// ouverture des fichiers
@@ -36,14 +42,18 @@ int main(int argc, char **argv){
 		else
 			shift = shift*10 + (temp-'0');
 	}
-	if(negative==0)	// le shift ne peut pas être négatif
+	if(negative==0){}	// le shift ne peut pas être négatif
 		// cas d'erreur
 	
-	if(temp=='E')	// si le fichier ne contient qu'un seul nombre
+	if(temp=='E'){	// si le fichier ne contient qu'un seul nombre
 		writeOutputOne(output,shift);
+		fclose(input);
+		fclose(output);
+		return 0;
+	}
 		
 	int first = 0;
-	nagative = 0;
+	negative = 0;
 	while((temp=fgetc(input))!='E' && temp!='\n' && temp!=EOF){
 		if(temp=='-')
 			negative = 0;
@@ -52,15 +62,16 @@ int main(int argc, char **argv){
 	}
 		
 	
-	if (temp=='E' || temp==EOF)	// si l'input n'est pas bon
+	if (temp=='E' || temp==EOF){}	// si l'input n'est pas bon
 		// mauvais input
 	
 	int second = 0;
 	while((temp=fgetc(input))!='E' && temp!='\n' && temp!=EOF)
 		first = first*10 + (temp-'0');
 	
+		
 	
-	
-	
+	fclose(input);
+	fclose(output);
 	return 0;
 }
