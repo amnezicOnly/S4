fd39835ef067472a7b8d934c6d32e666209fbaa0
Antoine Lévêque <antoine.leveque@epita.fr>
2024-03-08 10:57:57 -0800

    Avancement tp6 de C : gestion du premier cas (juste une valeur) + bon formattage de l'output)

--- BCIT/ProceduralProgramming/lab6/lab6.c
+++ BCIT/ProceduralProgramming/lab6/lab6.c
@@ -2,19 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int mainFunction(int shift, long lower, long upper, int first, int negative1, int second, int negative2){
+    // calcule first*second et regarde s'il y a dépassemeent
+    // si dépassement par les négatifs, retourne -1
+    // si dépassement par les positifs, retourne 1
+    // retourne 0 sinon
+    int result = first*second;
 
-void writeOutputOne(FILE* output, int shift){
-	// écrit les valeurs du minimum et du maximum en fonction de shift dans le fichier output
-	printf("Hello there\n");
-	int number = 0;
-	for(int i=0; i<shift-1; i++){
-		number<<=1;
-		number++;
-	}
-	fprintf(output,"min : -%d    %X\n",number,number);
-		
+
+    return 0;
 }
-
 
 
 int main(int argc, char **argv){
@@ -32,44 +29,94 @@ int main(int argc, char **argv){
 	if (output==NULL)
 		perror("Unable to open output file.\n");
 	
-	// récupération des valeurs
+	// récupération et traitement du shift
 	int shift = 0;
 	char temp;
-	int negative = 1;
-	while((temp=fgetc(input))!='E' && temp!='\n'){
-		if(temp=='-')
-			negative = 0;
-		else
+	while((temp=fgetc(input))!='E' && temp!='\n' && temp!='-')
 			shift = shift*10 + (temp-'0');
+
+    // si fichier vide
+    if(temp=='E' || temp==EOF){
+        fputs("Input file should contains exactly 1 or 3 values.\n",output);
+        fclose(input);
+        fclose(output);
+        return 0;
+    }
+
+	if(temp=='-'){	// le shift ne peut pas être négatif
+        fputs("The first value has to be strictly greater than 0.\n",output);
+        fclose(input);
+        fclose(output);
+        return 0;
+    }
+
+    // si le shift n'est pas compris entre 4 et 64
+    if(shift<4 || shift>64){
+        fputs("The first has to be strictly greater than 3 and strictly lower than 65.\n",output);
+        fclose(input);
+        fclose(output);
+        return 0;
+    }
+
+    // on calcule les bornes inférieures et supérieures
+    long upper = 1;
+	for(int i=1; i<shift-1; i++){
+		upper<<=1;
+        upper++;
 	}
-	if(negative==0){}	// le shift ne peut pas être négatif
-		// cas d'erreur
+
+    long lower = ~upper;
+
 	
-	if(temp=='E'){	// si le fichier ne contient qu'un seul nombre
-		writeOutputOne(output,shift);
-		fclose(input);
-		fclose(output);
-		return 0;
-	}
-		
 	int first = 0;
-	negative = 0;
+	int negative1 = 0;
 	while((temp=fgetc(input))!='E' && temp!='\n' && temp!=EOF){
-		if(temp=='-')
-			negative = 0;
+        if(temp=='-')
+			negative1 = 0;
 		else
 			first = first*10 + (temp-'0');
 	}
 		
-	
-	if (temp=='E' || temp==EOF){}	// si l'input n'est pas bon
+	if(temp=='E'){	// si le fichier ne contient qu'un seul nombre
+        fprintf(output,"min: %ld\t0x%.16lx\n",lower,lower);
+        fprintf(output,"max: %ld\t0x%.16lx\n",upper,upper);
+		fclose(input);
+		fclose(output);
+		return 0;
+	}
+
+	if (temp=='E' || temp==EOF){	// si l'input n'est pas bon
 		// mauvais input
-	
+        fputs("Input file should contains 1 or 3 values exactly.\n",output);
+        fclose(output);
+        fclose(input);
+        return 0;
+    }
+
+    if(negative1==1)
+        first = -first;
+
+
 	int second = 0;
-	while((temp=fgetc(input))!='E' && temp!='\n' && temp!=EOF)
-		first = first*10 + (temp-'0');
-	
-		
+    int negative2 = 0;
+	while((temp=fgetc(input))!='E' && temp!='\n' && temp!=EOF){
+        if(temp=='-')
+            negative2 = 1;
+        else
+		    first = first*10 + (temp-'0');
+    }
+
+    if(negative2==1)
+        second = -second;
+
+    int enfin = mainFunction(shift,upper,lower,first,negative1,second,negative2);
+
+    if(enfin==-1)
+        fprintf(output,"%ld\n",lower);
+    if(enfin==0)
+        fprintf(output,"%d\n",(first*second));
+    if(enfin==1)
+        fprintf(output,"%ld\n",upper);
 	
 	fclose(input);
 	fclose(output);
