3b23da89365bff9e883e09172729466acccad767
Antoine Lévêque <antoine.leveque@epita.fr>
2024-04-04 10:04:54 -0700

    pro:C: fin dm3 et tp8

---
+++ BCIT/ProceduralProgramming/Assignements/assignement3/assignment3.c
@@ -0,0 +1,200 @@
+// A01410991
+// Antoine Leveque
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Employee{
+	int ID;
+	char* name;
+	float salary;
+} Employee;
+
+int line = 1;
+
+void listEmployees(FILE* output, Employee* employees, size_t size){
+	for(size_t i=0; i<size; i++)
+		fprintf(output,"%d,%s,%.2f\n",employees[i].ID,employees[i].name,employees[i].salary);
+}
+
+void freeEmployees(Employee* employees, size_t size){
+	free(employees);
+}
+
+int getID(char id[]){
+	int res = 0;
+	for(size_t i = 0; id[i]!='\0'; i++){
+		if(id[i]<'0' || id[i]>'9')
+			return -1;
+		res*=10;
+		res+=id[i]-'0';
+	}
+	return res;
+}
+
+int checkName(char name[]){
+	int state = 0;	
+	for(size_t i = 0; name[i]!='\0' ; i++){
+		if(!((name[i]>='a' && name[i]<='z')||(name[i]>='A' && name[i]<='Z')) && state!=0)
+			return -1;
+		if(name[i]==' ')
+			state++;
+	}
+	if(state!=1)
+		return -1;
+	return 0;
+}
+
+float getSalary(char salary[]){
+	int res = 0;
+	int state = 0;
+	for(size_t i = 0; salary[i]!='\0'; i++){
+		if((salary[i]<'0' || salary[i]>'9') && state!=0)
+			return -1;
+		if(salary[i]=='.')
+			state++;
+		else{
+			res*=10;
+			res+=salary[i]-'0';
+		}
+	}
+	res = (float)res;
+	res/=100;
+	return res;
+} 
+
+
+int sortNames(char* name1, char* name2){
+	size_t i = 0;
+	while(name1[i]!='\0' && name2[i]!='\0' && name1[i]==name2[i])
+		i++;
+	
+	if(name1[i]=='\0' && name2[i]=='\0')
+		return 0;
+	if(name1[i]=='\0' && name2[i]!='\0')
+		return 1;
+	if(name1[i]!='\0' && name2[i]=='\0')
+		return -1;
+	
+	else{
+		if(name1[i]<name2[i])
+			return 1;
+		return -1;
+	}
+}
+
+int addToList(Employee** employees, size_t* size, int id, char Name[], float Salary){
+	for(size_t i=0; i<(*size); i++){
+		if((*employees)[i].ID==id)
+			return -1;
+	}
+	
+	if((*size)==0){
+		*employees = (Employee*)malloc(sizeof(Employee));
+		if(employees==NULL)
+			return -1;
+	}
+	else{
+		Employee* temp = (Employee*)realloc(*employees, (*size + 1) * sizeof(Employee));
+		if(temp==NULL)
+			return -1;
+		*employees = temp;
+	}
+		
+		
+	Employee* newEmployee = (Employee*)malloc(sizeof(Employee));
+	if(newEmployee==NULL)
+		return -1;
+	char* newName = (char*)malloc(strlen(Name) + 1);
+	if(newName == NULL)
+        	return -1;
+    	strcpy(newName, Name);
+	
+	newEmployee->ID = id;
+    	newEmployee->name = newName;
+    	newEmployee->salary = Salary;
+    	
+    	(*employees)[*size] = *newEmployee;
+    	(*size)++;
+    	
+    	size_t i = (*size)-1;
+    	while(i>0 && sortNames(((*employees)[i]).name, ((*employees)[i-1]).name)>0){
+		Employee temp = (*employees)[i];
+		(*employees)[i] = (*employees)[i-1];
+	    	(*employees)[i-1] = temp;
+	    	i--;
+	}
+	
+	while(i>0 && sortNames(((*employees)[i]).name, ((*employees)[i-1]).name)==0 && ((*employees)[i-1]).ID>((*employees)[i]).ID){
+		Employee temp = (*employees)[i];
+		
+		(*employees)[i] = (*employees)[i-1];
+	    	(*employees)[i-1] = temp;
+	    	i--;
+	}
+    	free(newEmployee);
+    	 return 0;
+}
+
+int catchData(FILE* input, Employee** employees, size_t* size){
+	char ligne[100];
+	int state = 0;
+	while (fgets(ligne, sizeof(ligne), input) != NULL){
+		if(ligne[0]=='E' && ligne[1]=='\n' && strlen(ligne)==2){
+			state++;
+			break;
+		}
+		char id[50], name[50], salary[50];
+		sscanf(ligne, "%[^,],%[^,],%s", id, name, salary);
+		
+		int ID = getID(id);
+		if(ID<0)
+			return -1;
+		
+		int nameCheck = checkName(name);
+		if(nameCheck<0)
+			return -1;
+		
+		float salaire = getSalary(salary);
+		if(salaire<0)
+			return -1;
+			
+		int addStatus = addToList(employees, size, ID, name, salaire);
+		if(addStatus<0)
+			return -1;
+		
+	}
+	return state;
+}
+
+
+int main(int argc, char** argv){
+	if(argc!=3)
+		return 1;
+	
+	FILE* input = fopen(argv[1],"r");
+	if(input==NULL)
+		return 1;
+	
+	FILE* output = fopen(argv[2],"w");
+	if(output==NULL)
+		return 1;
+		
+	Employee* employees = NULL;
+	size_t size = 0;
+	
+	int processStatus = catchData(input,&employees,&size);
+	if(processStatus<0 || size==0){
+		fputs("Error\n",output);
+		fclose(input);
+		fclose(output);
+		free(employees);
+		return 0;
+	}
+	
+	listEmployees(output,employees,size);
+	fclose(input);
+	fclose(output);
+	freeEmployees(employees,size);
+	return 0;
+}
--- BCIT/ProceduralProgramming/Labs/lab8/lab8.c
+++ BCIT/ProceduralProgramming/Labs/lab8/lab8.c
@@ -7,12 +7,6 @@
 #include <limits.h>
 #include <float.h>
 
-/*
-Mes questions :
-	- Si un élément est déjà dans la liste : on l'ajoute, on ne fait rien ou erreur ?
-	- 
-*/
-
 size_t size;
 char type;
 FILE* input;
@@ -22,16 +16,6 @@ typedef struct Node{
 	void* data;
 	struct Node* next;
 } Node;
-
-void printList(Node* head){
-	Node* curr = head;
-	while(curr!=NULL){
-		char* word = (char*) curr->data;
-		printf("%s,",word);
-		curr = curr->next;
-	}
-	printf("\n");
-}
 
 int compareNumbers(char* word1, char* word2){
 	if(type=='3'){
@@ -90,149 +74,138 @@ int compare(void* word1, void* word2){
 	}
 }
 
-// Fonction pour fusionner deux listes triées
 Node* merge(Node* list1, Node* list2) {
-	//printf("Left: ");
-	//printList(list1);
-	//printf("Right: ");
-	//printList(list2);
-    if (list1 == NULL) return list2;
-    if (list2 == NULL) return list1;
+    	if (list1 == NULL)
+    		return list2;
+    	if (list2 == NULL)
+    		return list1;
 
-    Node* result = NULL;
-    Node* current = NULL;
+    	Node* result = NULL;
+    	Node* current = NULL;
 
-    while (list1 != NULL && list2 != NULL) {
-	if(compare(list1->data,list2->data)<0){	// si list1->data<=list2->data
-            if (result == NULL) {
-                result = list1;
-                current = list1;
-            } else {
-                current->next = list1;
-                current = current->next;
-            }
-			// on avance le 
-            list1 = list1->next;
-        } else {
-            if (result == NULL) {
-                result = list2;
-                current = list2;
-            } else {
-                current->next = list2;
-                current = current->next;
-            }
-            list2 = list2->next;
-        }
-    }
+    	while (list1 != NULL && list2 != NULL) {
+		if(compare(list1->data,list2->data)<0){
+            		if (result == NULL) {
+                		result = list1;
+                		current = list1;
+            		} else {
+               			 current->next = list1;
+                		current = current->next;
+           		}
+           		list1 = list1->next;
+        	} else {
+            		if (result == NULL) {
+                		result = list2;
+               			current = list2;
+            		} else {
+                		current->next = list2;
+                		current = current->next;
+            		}
+            		list2 = list2->next;
+        	}
+    	}
 
-    if (list1 != NULL) {
-        current->next = list1;
-    } else {
-        current->next = list2;
-    }
-    
-    //printf("Result :");
-    //printList(result);
-
-    return result;
+    	if (list1 != NULL) {
+        	current->next = list1;
+    	} else {
+        	current->next = list2;
+    	}
+    	return result;
 }
 
-// Fonction pour diviser la liste en deux moitiés
+
 void splitList(Node* head, Node** left, Node** right) {
-    Node* fast;
-    Node* slow;
-    if (head == NULL || head->next == NULL) {
-        *left = head;
-        *right = NULL;
-    } else {
-        slow = head;
-        fast = head->next;
+    	Node* fast;
+    	Node* slow;
+    	
+    	if (head == NULL || head->next == NULL) {
+        	*left = head;
+        	*right = NULL;
+    	} else {
+        	slow = head;
+        	fast = head->next;
 
-        while (fast != NULL) {
-            fast = fast->next;
-            if (fast != NULL) {
-                slow = slow->next;
-                fast = fast->next;
-            }
-        }
+        	while (fast != NULL) {
+            		fast = fast->next;
+            		if (fast != NULL) {
+                		slow = slow->next;
+                		fast = fast->next;
+            		}
+        	}
 
-        *left = head;
-        *right = slow->next;
-        slow->next = NULL;
-    }
+        	*left = head;
+        	*right = slow->next;
+        	slow->next = NULL;
+	}
 }
 
 // Fonction principale de tri fusion
 void mergeSort(Node** linkedList) {
-    Node* head = *linkedList;
-    Node* a;
-    Node* b;
+	Node* head = *linkedList;
+	
+	Node* a;
+	Node* b;
+	
+	if (head == NULL || head->next == NULL) {
+		return;
+	}
 
-    if (head == NULL || head->next == NULL) {
-        return;
-    }
+	splitList(head, &a, &b);
 
-    splitList(head, &a, &b);
+	mergeSort(&a);
+	mergeSort(&b);
 
-    mergeSort(&a);
-    mergeSort(&b);
-
-    *linkedList = merge(a, b);
+	*linkedList = merge(a, b);
 }
 
-int checkShort(char* str, int negative) {
-    	char* endptr;
-    	long num = strtol(str+negative, &endptr, 20);
-    	if (endptr == NULL || *endptr != '\0') {
-        	return -1; // La chaîne ne représente pas un nombre valide
-    	}
-
-    	if (num < SHRT_MIN || num > SHRT_MAX) {
-    		//printf("erreur là\n");
-        	return -1; // Le nombre dépasse la plage des shorts
-    	}
-	return 1; // Le nombre est un short valide dans la plage autorisée
+int checkShort(char* source, int negative) {
+	char str[strlen(source) + 1];
+	sprintf(str, "%hd", ((short)atoi(source+negative)));
+	if(strcmp(str, source+negative) == 0)
+		return 1;
+	return -1;
 }
 
-int checkInt(char* str, int negative) {
+int checkInt(char* source, int negative) {/*
     	char* endptr;
     	long num = strtol(str+negative, &endptr, 20);
     	if (endptr == str || *endptr != '\0') {
-    	    return -1; // La chaîne ne représente pas un nombre valide
+    	    return -1;
     	}
 
     	if (num < INT_MIN || num > INT_MAX) {
-    	    return -1; // Le nombre dépasse la plage des int
+    	    return -1;
     	}
-	return 1; // Le nombre est un int valide dans la plage autorisée
+	return 1;*/
+	
+	char str[strlen(source) + 1];
+	sprintf(str, "%d", atoi(source+negative));
+	if(strcmp(str, source+negative) == 0)
+		return 1;
+	return -1;
 }
 
 int checkFloat(char* str, int negative) {
-    char* endptr;
-    float num = strtof(str+negative, &endptr);
-    if (endptr == str || *endptr != '\0') {
-        return -1; // La chaîne ne représente pas un nombre valide
-    }
+    	char* endptr;
+    	float num = strtof(str+negative, &endptr);
+    	if (endptr == str || *endptr != '\0') {
+        	return -1;
+    	}
 
-    if (num < -FLT_MAX || num > FLT_MAX) {
-        return -1; // Le nombre dépasse la plage des floats
-    }
+    	if (num < -FLT_MAX || num > FLT_MAX) {
+        	return -1;
+    	}
 
-    return 1; // Le nombre est un float valide dans la plage autorisée
+    	return 1;
 }
 
 
 void addData(Node** liste, void *dataptr, size_t size) {
-    	// Création d'un nouveau nœud
    	Node *newNode = (Node*)malloc(sizeof(Node));
     	if (newNode == NULL) {
-        // Gérer l'échec de l'allocation
         	exit(EXIT_FAILURE);
     	}
 
-	
-    
-    	// Affectation de la donnée pointée par dataptr au nouveau noeud
     	newNode->data = malloc(size);
 	memcpy(newNode->data, dataptr, size);
     	newNode->next = *liste;
@@ -241,54 +214,48 @@ void addData(Node** liste, void *dataptr, size_t size) {
 
 void writeInFile(Node* linkedList){
 	Node* curr = linkedList;
-	while(curr->next!=NULL){
-		Node* next = curr->next;
-		char* word = (char*)curr->data;
-		fprintf(output,"%s,",word);
-		curr = next;
+	while(curr!=NULL){
+		fprintf(output,"%s",(char*)curr->data);
+		if(curr->next!=NULL)
+			fprintf(output,",");
+		curr = curr->next;
 	}
-	char* word = (char*)curr->data;
-	fprintf(output,"%s\n",word);
+	fprintf(output,"\n");
 }
 
 int fileProcess(Node** linkedList){
 	char temp;
 	while((temp=fgetc(input))!='\n' && temp!=EOF){
-		int negative = 0;
 		int virgule = 0;
 		char* word = malloc(sizeof(char));
 		word[0] = temp;
+		int negative = 0;
+		if(temp=='-')
+			negative++;
 		size_t size = 1;
 		while((temp=fgetc(input))!=',' && temp!='\n' && temp!=EOF){
-			if(temp=='-'){
-				if(negative!=0)
-					return -1;
-				negative++; 
+			if(type<='3' && temp=='-'){
+				return -1;
 			}
 			if(temp=='.'){
 				if(virgule!=0)
 					return -1;
 				virgule++;
-			}	
+			}
 			word = realloc(word,(size+1)*sizeof(char));
 			word[size] = temp;
 			size++;
 		}
-		//printf("%s\n",word);
 		if((type=='4' && size!=1)||(type>='1' && type<='3' && size>=20))
 			return -1;
 
 		if(type=='1' && checkShort(word,negative)<0){
-			//printf("Error checkShort\n");
-			//printf("%s\n",word);
 			return -1;
 		}
 		if(type=='2' && checkInt(word,negative)<0){
-			//printf("Error checkInt\n");
 			return -1;
 		}
 		if(type=='3' && checkFloat(word,negative)<0){
-			//printf("Error checkFloat\n");
 			return -1;
 		}
 		addData(linkedList,word,size);
@@ -299,15 +266,28 @@ int fileProcess(Node** linkedList){
 	return 0;
 }
 
-int quitProgram(){
-    fclose(output);
-    fclose(input);
-    return 0;
+void freeLinkedList(Node* linkedList){
+	Node* curr = linkedList;
+	Node* next;
+	
+	while(curr!=NULL){
+		next = curr->next;
+		free(curr->data);
+		free(curr);
+		curr = next;
+	}
 }
 
-int quitError(){
-    fputs("Error\n",output);
-    return quitProgram(input,output);
+int quitProgram(Node* linkedList){
+	freeLinkedList(linkedList);
+    	fclose(output);
+    	fclose(input);
+    	return 0;
+}
+
+int quitError(Node* linkedList){
+    	fputs("Error\n",output);
+    	return quitProgram(linkedList);
 }
 
 int main(int argc, char** argv){
@@ -328,13 +308,11 @@ int main(int argc, char** argv){
 	// Récupération du type de données
 	type = fgetc(input);
 	if(type<'1' || type>'5'){
-		//printf("Mauvais type: %c (1)\n",type);
-		return quitError(input,output);
+		return quitError(linkedList);
 	}
 	// Si c'est un nombre et pas un chiffre
 	if (fgetc(input)!='\n'){
-        //printf("Type trop grand\n");
-        return quitError();
+        return quitError(linkedList);
     }
 
 	switch(type){
@@ -353,16 +331,15 @@ int main(int argc, char** argv){
 		case '5':
 			size = sizeof(char*);
 			break;
-		default :	// si type
-			//printf("Mauvais type: %c (2)\n",type);
-			return quitError();
+		default :
+			return quitError(linkedList);
 	}
 	int check = fileProcess(&linkedList);
 	if(check<0){
-		return quitError();
+		return quitError(linkedList);
 	}
 
 	mergeSort(&linkedList);
 	writeInFile(linkedList);
-	return quitProgram();
+	return quitProgram(linkedList);
 }
