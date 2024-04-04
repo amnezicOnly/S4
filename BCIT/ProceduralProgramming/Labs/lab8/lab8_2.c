f319811196c587e83db1a05309d9bdb9be49c305
Antoine Lévêque <antoine.leveque@epita.fr>
2024-04-03 17:45:27 -0700

    perso:C:tp8: le tri pour les nombres ne fonctionnent pas très bien : ex 32<333<34

--- BCIT/ProceduralProgramming/Labs/lab8/lab8_2.c
+++ BCIT/ProceduralProgramming/Labs/lab8/lab8_2.c
@@ -12,131 +12,61 @@ Mes questions :
 
 size_t size;
 char type;
-FILE* input;
-FILE* output;
+size_t capacity = 0;
 
 typedef struct Node{
 	void* data;
 	struct Node* next;
 } Node;
 
-void printList(Node* head){
-	Node* curr = head;
-	while(curr!=NULL){
-		char* word = (char*) curr->data;
-		printf("%s,",word);
-		curr = curr->next;
-	}
-	printf("\n");
-}
-
-
 int compare(void* word1, void* word2){
 	char* text1 = (char*)word1;
 	char* text2 = (char*)word2;
+	
 	size_t i = 0;
 	while(text1[i]!='\0' && text2[i]!='\0' && text1[i]==text2[i])
 		i++;
+
 	if(text1[i]=='\0' && text2[i]!='\0')
 		return -1;
 	if(text1[i]!='\0' && text2[i]=='\0')
 		return 1;
+
 	if(text1[i]!='\0' && text2[i]!='\0' && text1[i]<text2[i])
 		return -1;
+
 	if(text1[i]!='\0' && text2[i]!='\0' && text1[i]<text2[i])
 		return 1;
+
 	return 0;
 }
 
-// Fonction pour fusionner deux listes triées
-Node* merge(Node* list1, Node* list2) {
-	//printf("Left: ");
-	//printList(list1);
-	//printf("Right: ");
-	//printList(list2);
-    if (list1 == NULL) return list2;
-    if (list2 == NULL) return list1;
-
-    Node* result = NULL;
-    Node* current = NULL;
-
-    while (list1 != NULL && list2 != NULL) {
-	if(compare(list1->data,list2->data)<0){	// si list1->data<=list2->data
-            if (result == NULL) {
-                result = list1;
-                current = list1;
-            } else {
-                current->next = list1;
-                current = current->next;
-            }
-			// on avance le 
-            list1 = list1->next;
-        } else {
-            if (result == NULL) {
-                result = list2;
-                current = list2;
-            } else {
-                current->next = list2;
-                current = current->next;
-            }
-            list2 = list2->next;
-        }
-    }
-
-    if (list1 != NULL) {
-        current->next = list1;
-    } else {
-        current->next = list2;
-    }
-    
-    //printf("Result :");
-    //printList(result);
-
-    return result;
+void split(Node* liste, Node** left, Node** right, size_t size){
+	
 }
 
-// Fonction pour diviser la liste en deux moitiés
-void splitList(Node* source, Node** frontRef, Node** backRef) {
-    Node* fast;
-    Node* slow;
-    if (source == NULL || source->next == NULL) {
-        *frontRef = source;
-        *backRef = NULL;
-    } else {
-        slow = source;
-        fast = source->next;
-
-        while (fast != NULL) {
-            fast = fast->next;
-            if (fast != NULL) {
-                slow = slow->next;
-                fast = fast->next;
-            }
-        }
-
-        *frontRef = source;
-        *backRef = slow->next;
-        slow->next = NULL;
-    }
+void mergeSort(Node** linkedList){
+	Node* head = *linkedList;
+	
+	// Si la liste est vide ou ne contient qu'un élément elle est inutile ou déjà triée
+	if(head==NULL || head->next==NULL)
+		return;
+		
+	// Sinon, on crée deux nouvelles listes chainées nulles
+	Node* left;
+	Node* right;
+	
+	// On sépare les deux listes à partir de head
+	split(head,&left,&right,size);
+	
+	// On redivise les listes si possibles
+	mergeSort(&left);
+	mergeSort(&right);
+	
+	// On trie les 2 pour les fusionner
+	*head = fusion(left,right);
 }
 
-// Fonction principale de tri fusion
-void mergeSort(Node** headRef) {
-    Node* head = *headRef;
-    Node* a;
-    Node* b;
-
-    if (head == NULL || head->next == NULL) {
-        return;
-    }
-
-    splitList(head, &a, &b);
-
-    mergeSort(&a);
-    mergeSort(&b);
-
-    *headRef = merge(a, b);
-}
 
 int checkShort(const char* word) {
     long long number = strtoll(word, NULL, 10); // Convertir la chaîne en nombre entier long
@@ -203,9 +133,10 @@ void addData(Node** liste, void *dataptr, size_t size) {
 	memcpy(newNode->data, dataptr, size);
     newNode->next = *liste;
     *liste = newNode;
+    capacity++;
 }
 
-void writeInFile(Node* linkedList){
+void writeInFile(FILE* output, Node* linkedList){
 	Node* curr = linkedList;
 	while(curr->next!=NULL){
 		Node* next = curr->next;
@@ -214,52 +145,42 @@ void writeInFile(Node* linkedList){
 		curr = next;
 	}
 	char* word = (char*)curr->data;
-	fprintf(output,"%s\n",word);
 }
 
-int fileProcess(Node** linkedList){
+int fileProcess(FILE* input, Node** linkedList){
 	char temp;
 	while((temp=fgetc(input))!='\n' && temp!=EOF){
+		int state = 0;
 		int negative = 0;
 		int virgule = 0;
 		char* word = malloc(sizeof(char));
 		word[0] = temp;
 		size_t size = 1;
-		while((temp=fgetc(input))!=',' && temp!='\n' && temp!=EOF){
+		while((temp=fgetc(input))!=',' && temp!='\n' && temp!=EOF && type!='4'){
 			if(temp=='.')
-				virgule++;
-			if(temp=='-'){
-				if(negative!=0)
-					return -1;
-				negative++; 
-			}
-			if(temp==','){
-				if(virgule!=0)
-					return -1;
-				virgule++;
-			}	
+				virgule++;			
 			word = realloc(word,(size+1)*sizeof(char));
 			word[size] = temp;
 			size++;
 		}
-		if((type=='4' && size!=1)||(type>='1' && type<='3' && size>=20))
+		if((type=='4' && temp!=',')||(type>='1' && type<='3' && size>=20))
 			return -1;
 
 		if(type=='1'){
 			if(checkShort(word)<0){
-				printf("Error checkShort\n");
+				//printf("Error checkShort\n");
 				return -1;
 			}
 		}
 		if(type=='2'){
 			if(checkInt(word)<0){
-				printf("Error checkInt\n");
+				//printf("Error checkInt\n");
 				return -1;
 			}
 		}
 		if(type=='3'){
 			if(virgule!=1 || checkFloat(word)<0){
-				printf("Error checkFloat\n");
+				//printf("Error checkFloat\n");
 				return -1;
 			}
 		}
@@ -271,13 +192,13 @@ int fileProcess(Node** linkedList){
 	return 0;
 }
 
-int quitProgram(){
+int quitProgram(FILE* input, FILE* output){
     fclose(output);
     fclose(input);
     return 0;
 }
 
-int quitError(){
+int quitError(FILE* input, FILE* output){
     fputs("Error\n",output);
     return quitProgram(input,output);
 }
@@ -288,10 +209,10 @@ int main(int argc, char** argv){
 		perror("Error\n");
 	
 	// Gestion des fichiers
-	input = fopen(argv[1],"r");
+	FILE* input = fopen(argv[1],"r");
 	if(input==NULL)
 		perror("Error: unable to open input file\n");
-	output = fopen(argv[2],"w");
+	FILE* output = fopen(argv[2],"w");
 	if(output==NULL)
 		perror("Error: unable to open output file\n");
 	
@@ -306,7 +227,7 @@ int main(int argc, char** argv){
 	// Si c'est un nombre et pas un chiffre
 	if (fgetc(input)!='\n'){
         //printf("Type trop grand\n");
-        return quitError();
+        return quitError(input,output);
     }
 
 	switch(type){
@@ -327,16 +248,18 @@ int main(int argc, char** argv){
 			break;
 		default :	// si type
 			//printf("Mauvais type: %c (2)\n",type);
-			return quitError();
+			return quitError(input,output);
 	}
 	
-	int check = fileProcess(&linkedList);
+
+	int check = fileProcess(input,&linkedList);
 	if(check<0){
-		return quitError();
+		return quitError(input,output);
 	}
 
 	// ERROR
-	mergeSort(&linkedList);
-	writeInFile(linkedList);
-	return quitProgram();
+	//mergeSort(&linkedList);
+
+	writeInFile(output,linkedList);
+	return quitProgram(input,output);
 }
